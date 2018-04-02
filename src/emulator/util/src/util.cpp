e282c0da8af250bf27bb778cc2d1d7ea4053bdfc
VelocityRa <makren67@gmail.com>
2018-04-03 00:33:46 +0300

    [log] Delete log file on init

--- src/emulator/util/src/util.cpp
+++ src/emulator/util/src/util.cpp
@@ -62,7 +62,15 @@ void init_logging()
 		{
 			std::cerr << "failed to get working directory" << std::endl;
 		}
-		sinks.push_back(std::make_shared<spdlog::sinks::simple_file_sink_mt>(s_cwd + LOG_FILE_NAME));
+        const auto full_log_path = s_cwd + LOG_FILE_NAME;
+        
+#ifdef WIN32
+        DeleteFileW(full_log_path.c_str());
+#else
+        remove(full_log_path.c_str());
+#endif
+
+		sinks.push_back(std::make_shared<spdlog::sinks::simple_file_sink_mt>(full_log_path));
 	}
 	catch (const spdlog::spdlog_ex& ex)
 	{
