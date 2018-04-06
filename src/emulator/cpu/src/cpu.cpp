bf813f1e2c2c37f9e9ded5f5da31272ca21574d1
Francisco José García García <frangarcj@gmail.com>
2018-04-06 10:22:42 +0200

    Typo order

--- src/emulator/cpu/src/cpu.cpp
+++ src/emulator/cpu/src/cpu.cpp
@@ -222,17 +222,17 @@ uint32_t read_reg(CPUState &state, size_t index) {
     return value;
 }
 
-uint32_t read_pc(CPUState &state) {
+uint32_t read_sp(CPUState &state) {
     uint32_t value = 0;
-    const uc_err err = uc_reg_read(state.uc.get(), UC_ARM_REG_PC, &value);
+    const uc_err err = uc_reg_read(state.uc.get(), UC_ARM_REG_SP, &value);
     assert(err == UC_ERR_OK);
 
     return value;
 }
 
-uint32_t read_sp(CPUState &state) {
+uint32_t read_pc(CPUState &state) {
     uint32_t value = 0;
-    const uc_err err = uc_reg_read(state.uc.get(), UC_ARM_REG_SP, &value);
+    const uc_err err = uc_reg_read(state.uc.get(), UC_ARM_REG_PC, &value);
     assert(err == UC_ERR_OK);
 
     return value;
