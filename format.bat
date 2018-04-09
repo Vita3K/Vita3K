4e7c0cd45df6e41af59399a822a4bc0b94dd942b
Rinnegatamante <rinnegatamante@gmail.com>
2018-04-09 22:16:00 +0200

    Ported format.sh to Windows.

---
+++ format.bat
@@ -0,0 +1,7 @@
+cd src\emulator
+for /f %%f in ('dir *.cpp *.h /b/s') do clang-format -i %%f
+cd ..\gen-modules
+for /f %%f in ('dir *.cpp *.h /b/s') do clang-format -i %%f
+cd ..\native-tool
+for /f %%f in ('dir *.cpp *.h /b/s') do clang-format -i %%f
+cd ..\..
