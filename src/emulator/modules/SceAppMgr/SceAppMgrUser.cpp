7736ecbd75455ec268b99dc5db0d1e3047808edc
Rinnegatamante <rinnegatamante@gmail.com>
2018-04-17 17:34:34 +0200

    Added back error handling.

--- src/emulator/modules/SceAppMgr/SceAppMgrUser.cpp
+++ src/emulator/modules/SceAppMgr/SceAppMgrUser.cpp
@@ -17,6 +17,7 @@
 
 #include "SceAppMgrUser.h"
 #include <host/sfo.h>
+#include <psp2/appmgr.h>
 
 EXPORT(int, _sceAppMgrGetAppState) {
     return unimplemented("_sceAppMgrGetAppState");
@@ -56,6 +57,9 @@ EXPORT(int, sceAppMgrAppParamGetInt) {
 
 EXPORT(int, sceAppMgrAppParamGetString, int pid, int param, char *string, int length) {
     std::string res = get_data(host.sfo_handle, param);
+    if (res.length() <= 0){
+        return error(__func__, SCE_APPMGR_ERROR_INVALID);
+    }
     res.copy(string, length);
     return 0;
 }
