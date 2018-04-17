dd82cc050045371fbdc847b9aef6b0dd8ed4b037
VelocityRa <makren67@gmail.com>
2018-04-18 02:56:09 +0300

    gxm/format: Change TODO positions

--- src/emulator/gxm/src/gxm.cpp
+++ src/emulator/gxm/src/gxm.cpp
@@ -660,17 +660,15 @@ GLenum translate_internal_format(SceGxmTextureFormat src) {
     }
 
     switch (src) {
-        case SCE_GXM_TEXTURE_FORMAT_U5U6U5_RGB:
-            return GL_RGB;
         case SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_ABGR:
         case SCE_GXM_TEXTURE_FORMAT_U8U8U8_BGR:
+        case SCE_GXM_TEXTURE_FORMAT_U5U6U5_RGB:
             return GL_RGBA8;
         case SCE_GXM_TEXTURE_FORMAT_U4U4U4U4_ABGR:
             return GL_RGBA4;
-        case SCE_GXM_TEXTURE_FORMAT_U8_R111:
-        case SCE_GXM_TEXTURE_FORMAT_U8_111R:
-        case SCE_GXM_TEXTURE_FORMAT_U8_1RRR:
-            // TODO: this is inaccurate
+        case SCE_GXM_TEXTURE_FORMAT_U8_R111: // TODO: this is inaccurate
+        case SCE_GXM_TEXTURE_FORMAT_U8_111R: // TODO: this is inaccurate
+        case SCE_GXM_TEXTURE_FORMAT_U8_1RRR: // TODO: this is inaccurate
             return GL_INTENSITY8;
         default:
         {
@@ -690,17 +688,14 @@ GLenum translate_format(SceGxmTextureFormat src) {
         case SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_ABGR:
         case SCE_GXM_TEXTURE_FORMAT_U4U4U4U4_ABGR:
             return GL_RGBA;
-        case SCE_GXM_TEXTURE_FORMAT_U8_R111:
-            // TODO: this is inaccurate
+        case SCE_GXM_TEXTURE_FORMAT_U8_R111: // TODO: this is inaccurate
             return GL_RED;
         case SCE_GXM_TEXTURE_FORMAT_U5U6U5_RGB:
         case SCE_GXM_TEXTURE_FORMAT_U8U8U8_BGR:
             return GL_RGB;
-        case SCE_GXM_TEXTURE_FORMAT_U8_111R:
-            // TODO: this is inaccurate
+        case SCE_GXM_TEXTURE_FORMAT_U8_111R: // TODO: this is inaccurate
             return GL_ALPHA;
-        case SCE_GXM_TEXTURE_FORMAT_U8_1RRR:
-            // TODO: this is inaccurate
+        case SCE_GXM_TEXTURE_FORMAT_U8_1RRR: // TODO: this is inaccurate
             return GL_INTENSITY;
         default:
         {
