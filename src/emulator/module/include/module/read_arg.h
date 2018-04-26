e342531ca86578e35f7ae9845eb3f7e228e007b2
Peter Mackay <mackay.pete+git@gmail.com>
2018-04-26 19:54:36 +0100

    Specialise to handle 64-bit arguments in registers. (Untested.)

--- src/emulator/module/include/module/read_arg.h
+++ src/emulator/module/include/module/read_arg.h
@@ -22,15 +22,28 @@
 
 #include <cpu/functions.h>
 
+// Read variable from register.
+template <typename T>
+std::enable_if_t<sizeof(T) < 8, T> read_from_gpr(CPUState &cpu, const ArgLayout &arg) {
+    const uint32_t reg = read_reg(cpu, arg.offset);
+    return static_cast<T>(reg);
+}
+
+// Specialised for 64-bit reads, which span 2 registers.
+template <typename T>
+std::enable_if_t<sizeof(T) == 8, T> read_from_gpr(CPUState &cpu, const ArgLayout &arg) {
+    const uint64_t lo32 = read_reg(cpu, arg.offset);
+    const uint64_t hi32 = read_reg(cpu, arg.offset + 1);
+    const uint64_t both = lo32 | (hi32 << 32);
+    return both;
+}
+
 // Read variable from register or stack, as specified by arg layout.
 template <typename T>
 T read(CPUState &cpu, const ArgLayout &arg, const MemState &mem) {
     switch (arg.location) {
         case ArgLocation::gpr:
-        {
-            const uint32_t reg = read_reg(cpu, arg.offset);
-            return static_cast<T>(reg);
-        }
+            return read_from_gpr<T>(cpu, arg);
         case ArgLocation::stack:
         {
             const Address sp = read_sp(cpu);
