b6bac68d094955db64089475b99ed371910c6f92
Paco García <frangarcj@gmail.com>
2018-05-08 20:02:39 +0200

    Remove wrong assert

--- src/emulator/modules/SceGxm/SceGxm.cpp
+++ src/emulator/modules/SceGxm/SceGxm.cpp
@@ -1283,7 +1283,6 @@ EXPORT(int, sceGxmSetUniformDataF, void *uniformBuffer, const SceGxmProgramParam
     assert(uniformBuffer != nullptr);
     assert(parameter != nullptr);
     assert(parameter->container_index == 14);
-    assert(componentOffset == 0);
     assert(componentCount > 0);
     assert(sourceData != nullptr);
 
