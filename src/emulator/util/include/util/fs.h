256792670a04fcc320197ab4b8ab26b40a9e74ed
Rinnegatamante <rinnegatamante@gmail.com>
2018-05-11 14:02:57 +0200

    Fix Linux/mac build.

--- src/emulator/util/include/util/fs.h
+++ src/emulator/util/include/util/fs.h
@@ -1,4 +1,13 @@
 #pragma once
 
+#ifndef __APPLE__
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
+#else
+void rmkdir(const char *dir);
+namespace fs {
+    bool create_directory(std::string path);
+    bool create_directories(std::string path);
+    int remove(std::string path);
+}
+#endif
