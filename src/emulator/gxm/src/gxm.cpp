8702aa079c9f9f49b9b9c7a159a333b1de99d54b
Peter Mackay <mackay.pete+git@gmail.com>
2018-05-14 18:23:38 +0100

    More thorough `translate_internal_format`.
    Just a copy of `translate_format` for now, as AFAICT we don't need to specify the internal format for the driver.

--- src/emulator/gxm/src/gxm.cpp
+++ src/emulator/gxm/src/gxm.cpp
@@ -931,28 +931,69 @@ namespace texture {
 
     GLenum translate_internal_format(SceGxmTextureFormat src) {
         GXM_PROFILE(__FUNCTION__);
-
-        if (is_paletted_format(src)) {
+        
+        const SceGxmTextureBaseFormat base_format = get_base_format(src);
+        switch (base_format) {
+            // 1 Component.
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_F16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_F32:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_F32M:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U32:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S32:
+            return GL_RED;
+            
+            // 2 components (red-green.)
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U8U8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S8S8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U16U16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S16S16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_F16F16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_F32F32:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U32U32:
+            return GL_RG;
+            
+            // 2 components (depth-stencil.)
+        case SCE_GXM_TEXTURE_BASE_FORMAT_X8U24:
+            return GL_DEPTH_STENCIL;
+            
+            // 3 components.
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U5U6U5:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S5S5U6:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_X8S8S8U8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_SE5M9M9M9:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_F11F11F10:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U8U8U8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S8S8S8:
+            return GL_RGB;
+            
+            // 4 components.
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U4U4U4U4:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U8U3U3U2:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U1U5U5U5:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U8U8U8U8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S8S8S8S8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U2U10U10U10:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_F16F16F16F16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U16U16U16U16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_S16S16S16S16:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_PVRT2BPP:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_PVRT4BPP:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_PVRTII2BPP:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_PVRTII4BPP:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_UBC1:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_UBC2:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_UBC3:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_YUV420P2:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_YUV420P3:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_YUV422:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_P4:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_P8:
+        case SCE_GXM_TEXTURE_BASE_FORMAT_U2F10F10F10:
             return GL_RGBA;
-        }
-
-        switch (src) {
-        case SCE_GXM_TEXTURE_FORMAT_U5U6U5_RGB:
-        case SCE_GXM_TEXTURE_FORMAT_U5U6U5_BGR:
-            return GL_RGB565;
-        case SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_ABGR:
-        case SCE_GXM_TEXTURE_FORMAT_U8U8U8_BGR:
-            return GL_RGBA8;
-        case SCE_GXM_TEXTURE_FORMAT_U4U4U4U4_ABGR:
-            return GL_RGBA4;
-        case SCE_GXM_TEXTURE_FORMAT_U1U5U5U5_ABGR:
-            return GL_RGB5_A1;
-        case SCE_GXM_TEXTURE_FORMAT_U8_R111: // TODO: this is inaccurate
-        case SCE_GXM_TEXTURE_FORMAT_U8_111R: // TODO: this is inaccurate
-        case SCE_GXM_TEXTURE_FORMAT_U8_1RRR: // TODO: this is inaccurate
-            return GL_R8;
-        default:
-            return GL_RGBA8;
         }
     }
 
