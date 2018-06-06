6c678a0a8b80c0f4ca19939b449724be6ebf690b
Peter Mackay <mackay.pete+git@gmail.com>
2018-06-06 09:15:56 +0100

    Integrate CMake and Conan.

--- gen.bat
+++ gen.bat
@@ -1,4 +1,5 @@
 mkdir build-windows
 pushd build-windows
+conan install --build missing ../src/external
 cmake -G "Visual Studio 15 2017 Win64" ..
 popd
--- gen.sh
+++ gen.sh
@@ -3,4 +3,5 @@ set -ex
 
 mkdir -p build-macos
 cd build-macos
+conan install --build missing ../src/external
 cmake -G Xcode ..
