5dec8beb0b1adfb07204f1aad26181b3e7a8f54d
Peter Mackay <mackay.pete+git@gmail.com>
2018-08-08 08:58:41 +0100

    host: Don't set image size -- the image isn't yet valid

--- src/emulator/host/src/host.cpp
+++ src/emulator/host/src/host.cpp
@@ -152,7 +152,6 @@ bool init(HostState &state) {
 
     state.base_path = base_path.get();
     state.pref_path = pref_path.get();
-    state.display.set_dims(DEFAULT_RES_WIDTH, DEFAULT_RES_HEIGHT);
     state.window = WindowPtr(SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DEFAULT_RES_WIDTH, DEFAULT_RES_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE), SDL_DestroyWindow);
     if (!state.window || !init(state.mem) || !init(state.audio, resume_thread) || !init(state.io, state.pref_path.c_str())) {
         return false;
