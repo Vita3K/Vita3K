cd4207b6e9cb7fc3f5f167e94a4a35a654cd2153
VelocityRa <makren67@gmail.com>
2018-08-25 23:39:11 +0300

    build: Boost integration
    Currently the ext-boost repo contains:
    - Boost.ProgramOptions
    - Boost.Filesystem
    - Boost.System
    
    No direct usage for the last one yet, but it may be and it was already
    a dependency by another lib anyway, so I included it in the "guaranteed"
    libraries in the `bcp` command.

--- gen.sh
+++ gen.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 set -ex
 
+if [[ -z "${CI}" ]]; then
+	mkdir src/external/boost-build
+	cd src/external/boost
+	b2 -j5 --build-dir=../boost-build
+	cd ../../..
+fi
+
+
 mkdir -p build-macos
 cd build-macos
 cmake -G Xcode ..
