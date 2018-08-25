ccd21c8e28dbec697196676f7f35d555d49130a3
VelocityRa <makren67@gmail.com>
2018-08-25 23:39:11 +0300

    build: Boost integration
    Currently the ext-boost repo contains:
    - Boost.ProgramOptions
    - Boost.Filesystem
    - Boost.System
    
    No direct usage for the last one yet, but it may be and it was already
    a dependency by another lib anyway, so I included it in the "guaranteed"
    libraries in the `bcp` command.

--- gen.bat
+++ gen.bat
@@ -1,3 +1,11 @@
+
+IF "%CI%"=="" (
+	mkdir src/external/boost-build
+	cd src/external/boost
+	b2 -j5 --stagedir=../boost-build stage
+	cd ../../..
+)
+
 mkdir build-windows
 pushd build-windows
 cmake -G "Visual Studio 15 2017 Win64" ..
