d5924bce9099f3f8f0792a55b0e38014f13fe19c
VelocityRa <makren67@gmail.com>
2018-08-29 20:24:52 +0300

    ci/travis: Change prebuilt boost lib

--- gen.sh
+++ gen.sh
@@ -4,6 +4,7 @@ set -ex
 if [[ -z "${CI}" ]]; then
 	mkdir src/external/boost-build
 	cd src/external/boost
+	bootstrap.sh   # Non-windows needs to build Boost.Build first (but b2.exe is already in ext-boost)
 	b2 -j5 --build-dir=../boost-build
 	cd ../../..
 fi
