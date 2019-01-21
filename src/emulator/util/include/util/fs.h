dff9a085eb3049604b1851bbdf38e00615cfd0c4
Taylor Whatley <1whatleytay@hdsb.ca>
2019-01-20 21:03:59 -0500

    util: filesystem fix for macOS (#388)
    * util: change util/fs to always use boost
    
    * util: deletes second filesystem include

--- src/emulator/util/include/util/fs.h
+++ src/emulator/util/include/util/fs.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#ifndef __APPLE__
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#else
-void rmkdir(const char *dir);
-namespace fs {
-bool create_directory(std::string path);
-bool create_directories(std::string path);
-int remove(std::string path);
-} // namespace fs
-#endif
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
