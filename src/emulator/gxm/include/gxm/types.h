bb12d6025b310f2bfe0465fee0ed80893128dd72
hgen264 <fewdspuckrito@gmail.com>
2019-03-20 22:06:17 +0700

    gxm: Change variable maybe_literals_offset to literals_offset
    Introducing Sure, brother of Not Sure!! Hii Not Sure-san!

--- src/emulator/gxm/include/gxm/types.h
+++ src/emulator/gxm/include/gxm/types.h
@@ -298,7 +298,7 @@ public:
     std::uint32_t unk_6C;
 
     std::uint32_t literals_count;
-    std::uint32_t maybe_literals_offset; //not sure
+    std::uint32_t literals_offset;
     std::uint32_t unk_78;
     std::uint32_t maybe_parameters_offset2; //not sure
 
