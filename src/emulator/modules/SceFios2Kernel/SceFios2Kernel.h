8f740332c540ebfc1b596ba51a5aec52d9852016
Zangetsu38 <Dante38@free.fr>
2019-03-30 02:11:00 +0100

    modules: Add SceFios2Kernel module and stub function.

---
+++ src/emulator/modules/SceFios2Kernel/SceFios2Kernel.h
@@ -0,0 +1,41 @@
+// Vita3K emulator project
+// Copyright (C) 2019 Vita3K team
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#pragma once
+
+#include <module/module.h>
+
+BRIDGE_DECL(_sceFiosKernelOverlayAdd)
+BRIDGE_DECL(_sceFiosKernelOverlayAddForProcess)
+BRIDGE_DECL(_sceFiosKernelOverlayDHChstatSync)
+BRIDGE_DECL(_sceFiosKernelOverlayDHCloseSync)
+BRIDGE_DECL(_sceFiosKernelOverlayDHOpenSync)
+BRIDGE_DECL(_sceFiosKernelOverlayDHReadSync)
+BRIDGE_DECL(_sceFiosKernelOverlayDHStatSync)
+BRIDGE_DECL(_sceFiosKernelOverlayDHSyncSync)
+BRIDGE_DECL(_sceFiosKernelOverlayGetInfo)
+BRIDGE_DECL(_sceFiosKernelOverlayGetInfoForProcess)
+BRIDGE_DECL(_sceFiosKernelOverlayGetList)
+BRIDGE_DECL(_sceFiosKernelOverlayGetRecommendedScheduler)
+BRIDGE_DECL(_sceFiosKernelOverlayModify)
+BRIDGE_DECL(_sceFiosKernelOverlayModifyForProcess)
+BRIDGE_DECL(_sceFiosKernelOverlayRemove)
+BRIDGE_DECL(_sceFiosKernelOverlayRemoveForProcess)
+BRIDGE_DECL(_sceFiosKernelOverlayResolveSync)
+BRIDGE_DECL(_sceFiosKernelOverlayResolveWithRangeSync)
+BRIDGE_DECL(_sceFiosKernelOverlayThreadIsDisabled)
+BRIDGE_DECL(_sceFiosKernelOverlayThreadSetDisabled)
