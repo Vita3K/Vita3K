e96bc4d57b8c9425a68799965a3ae86b081409a6
Zangetsu38 <Dante38@free.fr>
2019-04-27 19:46:20 +0200

    Gui: Add size icon choice in config. Gui: prepare game icon tab.

--- src/emulator/config.cpp
+++ src/emulator/config.cpp
@@ -17,6 +17,7 @@
 
 #include "config.h"
 
+#include <gui/state.h>
 #include <host/version.h>
 #include <util/log.h>
 #include <util/string_utils.h>
@@ -95,6 +96,7 @@ bool serialize(Config &cfg) {
     config_file_emit_single(emitter, "log-uniforms", cfg.log_uniforms);
     config_file_emit_single(emitter, "pstv-mode", cfg.pstv_mode);
     config_file_emit_single(emitter, "show-gui", cfg.show_gui);
+    config_file_emit_single(emitter, "icon-size", cfg.icon_size);
     config_file_emit_single(emitter, "archive-log", cfg.archive_log);
     config_file_emit_vector(emitter, "lle-modules", cfg.lle_modules);
     config_file_emit_optional_single(emitter, "log-level", cfg.log_level);
@@ -130,6 +132,7 @@ static bool deserialize(Config &cfg) {
     get_yaml_value(config_node, "log-uniforms", &cfg.log_uniforms, false);
     get_yaml_value(config_node, "pstv-mode", &cfg.pstv_mode, false);
     get_yaml_value(config_node, "show-gui", &cfg.show_gui, false);
+    get_yaml_value(config_node, "icon-size", &cfg.icon_size, static_cast<int>(gui::ICON_SIZE_DEFAULT));
     get_yaml_value(config_node, "archive-log", &cfg.archive_log, false);
     get_yaml_value_optional(config_node, "log-level", &cfg.log_level, static_cast<int>(spdlog::level::trace));
     get_yaml_value_optional(config_node, "pref-path", &cfg.pref_path);
