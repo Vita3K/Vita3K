da15cd0ff1b348d5d6298ff9a1055c5d731310f7
pent0 <fewdspuckrito@gmail.com>
2019-05-23 09:27:39 +0700

    shader/recompiler: Remove static temp reg vars creation

--- src/emulator/shader/src/spirv_recompiler.cpp
+++ src/emulator/shader/src/spirv_recompiler.cpp
@@ -807,15 +807,6 @@ static SpirvShaderParameters create_parameters(spv::Builder &b, const SceGxmProg
         create_fragment_output(b, spv_params, program);
     }
 
-    // Create temp reg vars
-    const std::size_t total_temp_reg_v4 = (program.temp_reg_count1 / 4) + ((program.temp_reg_count1 % 4) ? 1 : 0);
-
-    for (auto i = 0; i < total_temp_reg_v4; i++) {
-        auto name = fmt::format("r{}", i * 4);
-        auto type = b.makeVectorType(b.makeFloatType(32), 4); // TODO: Figure out correct type
-        create_spirv_var_reg(b, spv_params, name, usse::RegisterBank::TEMP, 4, type);
-    }
-
     // Create internal reg vars
     for (auto i = 0; i < 3; i++) {
         auto name = fmt::format("i{}", i);
