1903492bda079a096cede043b016ba7a29b21cf5
pent0 <fewdspuckrito@gmail.com>
2019-07-24 13:02:57 +0700

    features: Add pack/unpack through extension feature

--- src/emulator/features/include/features/state.h
+++ src/emulator/features/include/features/state.h
@@ -2,6 +2,7 @@
 
 struct FeatureState {
     bool direct_pack_unpack_half = false;
+    bool pack_unpack_half_through_ext = false;
     bool support_shader_interlock = false;      ///< First option for blending. Using this with ordered execution mode.
     bool support_texture_barrier = false;       ///< Second option for blending. Slower but work on 3 vendors.
     bool direct_fragcolor = false;
