6aec9ef056764780f2ada67928dad30b46ba7560
Francisco José García García <frangarcj@gmail.com>
2019-07-30 19:14:32 +0200

    Update src/emulator/modules/SceLibKernel/SceLibKernel.cpp
    Co-Authored-By: Nick Renieris <velocityra@gmail.com>

--- src/emulator/modules/SceLibKernel/SceLibKernel.cpp
+++ src/emulator/modules/SceLibKernel/SceLibKernel.cpp
@@ -982,7 +982,7 @@ EXPORT(int, sceKernelGetModuleInfoByAddr, Ptr<void> addr, emu::SceKernelModuleIn
     KernelState *const state = &host.kernel;
 
     for (SceKernelModuleInfoPtrs::iterator module = state->loaded_modules.begin(); module != state->loaded_modules.end(); ++module){
-        for(int n = 0; n < MODULE_INFO_NUM_SEGMENTS; n++) {
+        for (int n = 0; n < MODULE_INFO_NUM_SEGMENTS; n++) {
             const auto segment_address_begin = module->second->segments[n].vaddr.address();
             const auto segment_address_end = segment_address_begin+module->second->segments[n].memsz;
             if (addr.address() >segment_address_begin && addr.address() < segment_address_end) {
