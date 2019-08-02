6be4d8792243a4e1fb2ec91a915da1be930b6b34
scribam <scribam@users.noreply.github.com>
2019-08-02 21:23:56 +0200

    travis: Use gcc docker image

--- .travis/linux/build.sh
+++ .travis/linux/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 
-docker pull ubuntu:18.04
-docker run -v $(pwd):/Vita3K -v "$HOME/.ccache":/root/.ccache ubuntu:18.04 /bin/bash -ex /Vita3K/.travis/linux/docker.sh
+docker run -v $(pwd):/Vita3K -v "$HOME/.ccache":/root/.ccache gcc:8 /bin/bash -ex /Vita3K/.travis/linux/docker.sh
 
 mkdir "$HOME/linux-build"
 cp -r $(pwd)/build/bin "$HOME/linux-build"
