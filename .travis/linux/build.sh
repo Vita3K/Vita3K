480a6c74871cb0f65d3999e7daacae6997757fcb
scribam <scribam@users.noreply.github.com>
2019-08-03 07:57:50 +0200

    travis: return error code from build.sh to Travis
    This way, if compilation failed, Travis will reflect this status

--- .travis/linux/build.sh
+++ .travis/linux/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-docker run -v $(pwd):/Vita3K -v "$HOME/.ccache":/root/.ccache gcc:8 /bin/bash -ex /Vita3K/.travis/linux/docker.sh
+docker run -v $(pwd):/Vita3K -v "$HOME/.ccache":/root/.ccache gcc:8 /bin/bash -ex /Vita3K/.travis/linux/docker.sh || exit $?
 
 mkdir "$HOME/linux-build"
 cp -r $(pwd)/build/bin "$HOME/linux-build"
