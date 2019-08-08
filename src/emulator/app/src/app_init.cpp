bc4a237d56af0c91723a060c1efd1f84e97d5891
pent0 <fewdspuckrito@gmail.com>
2019-08-08 19:09:42 +0700

    app/init: Warn about shader interlock bug on RTX

--- src/emulator/app/src/app_init.cpp
+++ src/emulator/app/src/app_init.cpp
@@ -270,9 +270,9 @@ bool init(HostState &state, Config cfg, const Root &root_paths) {
 
     // Workaround driver bugs
     const std::string gpu_name = reinterpret_cast<const GLchar*>(glGetString(GL_RENDERER));
+    const bool is_rtx = gpu_name.find("GeForce RTX") != std::string::npos;
 
-    if (gpu_name.find("GeForce RTX") != std::string::npos || gpu_name.find("RTX") != std::string::npos ||
-        gpu_name.find("rtx") != std::string::npos) {
+    if (is_rtx) {
         // Disable shader interlock for all drivers.
         // TODO: Report and fix this on NVIDIA GeForce GTX driver
         state.features.support_shader_interlock = false; 
@@ -284,7 +284,11 @@ bool init(HostState &state, Config cfg, const Root &root_paths) {
         LOG_INFO("Your GPU supports shader interlock, some games that use programmable blending will have better performance.");
     } else if (state.features.support_texture_barrier) {
         LOG_INFO("Your GPU only supports texture barrier, performance may not be good on programmable blending games.");
-        LOG_WARN("Consider updating to GPU that has shader interlock.");
+        
+        if (is_rtx)
+            LOG_WARN("Shader interlock on GeForce RTX GPU driver is disabled due to a bug in the driver pending to be fixed.");
+        else
+            LOG_WARN("Consider updating to GPU that has shader interlock.");
     } else {
         LOG_INFO("Your GPU doesn't support extensions that make programmable blending possible. Some games may have broken graphics.");
         LOG_WARN("Consider updating your graphics drivers or upgrading your GPU.");
