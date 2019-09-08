044f0835efc00c955e8c8370c1dab9f07f4383fd
pent0 <fewdspuckrito@gmail.com>
2019-09-08 12:37:23 +0700

    renderer: Fix artifacts on RTX driver
    Write of last draw call wasn't visible to new draw call, imageStore would just create race cond. Use memory barrier!

--- vita3k/renderer/src/gl/renderer.cpp
+++ vita3k/renderer/src/gl/renderer.cpp
@@ -275,13 +275,8 @@ bool create(WindowPtr &window, std::unique_ptr<State> &state) {
         }
     }
 
-    const bool is_rtx = gpu_name.find("GeForce RTX") != std::string::npos;
-
-    if (is_rtx) {
-        // Disable shader interlock for all drivers.
-        // TODO: Report and fix this on NVIDIA GeForce GTX driver
-        gl_state.features.support_shader_interlock = false;
-    }
+    // Workaround driver bugs
+    const std::string gpu_name = reinterpret_cast<const GLchar *>(glGetString(GL_RENDERER));
 
     if (gl_state.features.direct_fragcolor) {
         LOG_INFO("Your GPU supports direct access to last fragment color. Your performance with programmable blending games will be optimized.");
@@ -289,12 +284,7 @@ bool create(WindowPtr &window, std::unique_ptr<State> &state) {
         LOG_INFO("Your GPU supports shader interlock, some games that use programmable blending will have better performance.");
     } else if (gl_state.features.support_texture_barrier) {
         LOG_INFO("Your GPU only supports texture barrier, performance may not be good on programmable blending games.");
-
-        if (is_rtx) {
-            LOG_WARN("Shader interlock on GeForce RTX GPU driver is disabled due to a bug in the driver pending to be fixed.");
-        } else {
-            LOG_WARN("Consider updating to GPU that has shader interlock.");
-        }
+        LOG_WARN("Consider updating to GPU that has shader interlock.");
     } else {
         LOG_INFO("Your GPU doesn't support extensions that make programmable blending possible. Some games may have broken graphics.");
         LOG_WARN("Consider updating your graphics drivers or upgrading your GPU.");
