21e0648bcdb90e623188faf5d5fcebf2233aecd0
IllusionMan1212 <hisham.abourgheba@gmail.com>
2019-11-02 01:30:01 +0200

    gui added a confirmation dialog when firmware installation is finished

--- vita3k/gui/src/install_firmware_dialog.cpp
+++ vita3k/gui/src/install_firmware_dialog.cpp
@@ -28,18 +28,40 @@
 
 namespace gui {
 
+static bool draw_file_dialog = true;
+
 void draw_install_firmware_dialog(GuiState &gui, HostState &host) {
     nfdchar_t *outpath = nullptr;
-    nfdresult_t result = NFD_OpenDialog("PUP", nullptr, &outpath);
+    nfdresult_t result = NFD_CANCEL;
 
-    if (result == NFD_OKAY) {
-        install_pup(outpath, host.pref_path + "/PUP_DEC");
-        get_modules_list(gui, host);
-        gui.file_menu.install_firmware_dialog = false;
-    } else if (result == NFD_CANCEL) {
-        gui.file_menu.install_firmware_dialog = false;
-    } else {
-        LOG_ERROR("Error initializing file dialog: {}", NFD_GetError());
+    const auto BUTTON_SIZE = ImVec2(60.f, 0.f);
+
+    if (draw_file_dialog) {
+        result = NFD_OpenDialog("PUP", nullptr, &outpath);
+        draw_file_dialog = false;
+
+        if (result == NFD_OKAY) {
+            install_pup(outpath, host.pref_path);
+        } else if (result == NFD_CANCEL) {
+            gui.file_menu.install_firmware_dialog = false;
+            draw_file_dialog = true;
+        } else {
+            LOG_ERROR("Error initializing file dialog: {}", NFD_GetError());
+            gui.file_menu.install_firmware_dialog = false;
+            draw_file_dialog = true;
+        }
     }
+
+    ImGui::OpenPopup("Firmware Installation");
+    if (ImGui::BeginPopupModal("Firmware Installation", nullptr, ImGuiWindowFlags_NoResize)) {
+        ImGui::TextColored(GUI_COLOR_TEXT, "Successfully Installed Firmware");
+        ImGui::SetCursorPosX(ImGui::GetWindowWidth() - BUTTON_SIZE.x - 10);
+        if (ImGui::Button("OK", BUTTON_SIZE)) {
+            get_modules_list(gui, host);
+            gui.file_menu.install_firmware_dialog = false;
+            draw_file_dialog = true;
+        }
+    }
+    ImGui::EndPopup();
 }
 } // namespace gui
