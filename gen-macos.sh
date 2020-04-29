f557ea739ed514e3fd80c6fcd574a1c82667a561
ichisadashioko <ichisadashioko@gmail.com>
2020-04-30 02:12:28 +0700

    vita3k: update build instructions (#686)
    - Turn off USE_DISCORD_RICH_PRESENCE because the binaries are not always
    compatible with Linux build.
    - Update build instructions for Linux, Windows and macOS
    - Update ext-boost for fixing missing headers

--- gen-macos.sh
+++ gen-macos.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
-BOOST_FOUND="$(cmake --find-package -DNAME=Boost -DCOMPILER_ID=GNU -DLANGUAGE=C -DMODE=EXIST)"
-
 # CI uses pre-built Boost
-if [[ -z "${CI}" && ${BOOST_FOUND} != "Boost found." ]]; then
+if [[ -z "${CI}" ]]; then
 	# Create build dir
 	mkdir -p external/boost-build
 	cd external/boost
 
-	# Non-Windows needs to build Boost.Build first
 	chmod +x tools/build/src/engine/build.sh
 	sh bootstrap.sh
 
 	# Build our Boost subset
-	./b2 -j5 --build-dir=../boost-build --stagedir=../boost-build stage
+	./b2 -j$(sysctl -n hw.logicalcpu) --build-dir=../boost-build --stagedir=../boost-build stage
 	cd ../..
 fi
 
