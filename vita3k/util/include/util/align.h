6241e7fbf5d5318bfcfa94738276fd38683086ce
Zangetsu38 <Dante38@free.fr>
2020-05-16 17:56:04 +0200

    modules/SceGxm: implement sceGxmTextureInitSwizzledArbitrary.

--- vita3k/util/include/util/align.h
+++ vita3k/util/include/util/align.h
@@ -12,3 +12,5 @@ template <typename T, typename = std::enable_if_t<std::is_integral<T>::value>>
 constexpr T align_down(const T &value, std::uint64_t align) {
     return static_cast<T>(value & ~(align - 1));
 }
+
+std::uint32_t nearest_power_of_two(std::uint32_t num);
