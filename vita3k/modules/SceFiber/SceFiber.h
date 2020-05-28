b396641e516a24cf20ab381ef94ab0932d4cdf79
Zangetsu38 <Dante38@free.fr>
2020-05-28 02:15:54 +0200

    modules/SceFiber: fix sceFiberGetSelf.

--- vita3k/modules/SceFiber/SceFiber.h
+++ vita3k/modules/SceFiber/SceFiber.h
@@ -19,6 +19,34 @@
 
 #include <module/module.h>
 
+enum SceFiberErrorCode {
+    SCE_FIBER_OK = 0x00000000, //!< Success
+    SCE_FIBER_ERROR_NULL = 0x80590001, //!< Some parameters are NULL.
+    SCE_FIBER_ERROR_ALIGNMENT = 0x80590002, //!< Some pointer-parameters are not aligned in their proper alignments.
+    SCE_FIBER_ERROR_RANGE = 0x80590003, //!< A parameter exceeds its range in the specification.
+    SCE_FIBER_ERROR_INVALID = 0x80590004, //!< A parameter has an invalid value.
+    SCE_FIBER_ERROR_PERMISSION = 0x80590005, //!< The function was called from the entity which does not have the permission.
+    SCE_FIBER_ERROR_STATE = 0x80590006, //!< The function was applied to a fiber in the state which the function does not support.
+    SCE_FIBER_ERROR_BUSY = 0x80590007, //!< The module specified by the function is busy.
+    SCE_FIBER_ERROR_AGAIN = 0x80590008, //!< The function could not complete because of the situation. Please try again later.
+    SCE_FIBER_ERROR_FATAL = 0x80590009, //!< The fiber caused an unrecoverable error.
+};
+
+typedef void(SceFiberEntry)(SceUInt32 argOnInitialize, SceUInt32 argOnRun);
+
+struct SceFiberOptParam {
+    char reserved[128];
+};
+
+typedef struct SceFiber {
+    Ptr<SceFiberEntry> entry;
+    SceUInt32 argOnInitialize;
+    Address addrContext;
+    SceSize sizeContext;
+    char name[32];
+    CPUContext cpu;
+} SceFiber;
+
 BRIDGE_DECL(_sceFiberAttachContextAndRun)
 BRIDGE_DECL(_sceFiberAttachContextAndSwitch)
 BRIDGE_DECL(_sceFiberInitializeImpl)
