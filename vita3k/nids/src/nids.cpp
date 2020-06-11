52e41f3bd2f90c5fda2d48d39bb2d1d44710c4b7
Sunho Kim <ksunhokim123@naver.com>
2020-06-11 16:05:19 +0900

    modules: fix hle var import and _pLibPerfCaptureFlagPtr

--- vita3k/nids/src/nids.cpp
+++ vita3k/nids/src/nids.cpp
@@ -1,16 +1,22 @@
 #include <nids/functions.h>
 
+#define VAR_NID(name, nid) extern const char name_##name[] = #name;
 #define NID(name, nid) extern const char name_##name[] = #name;
 #include <nids/nids.h>
 #undef NID
+#undef VAR_NID
 
 const char *import_name(uint32_t nid) {
     switch (nid) {
+#define VAR_NID(name, nid) \
+    case nid:          \
+        return name_##name;
 #define NID(name, nid) \
     case nid:          \
         return name_##name;
 #include <nids/nids.h>
 #undef NID
+#undef VAR_NID
     default:
         return "UNRECOGNISED";
     }
