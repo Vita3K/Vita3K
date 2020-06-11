28eb3d6b8a05b1e1aaca0e5d3231f86453b29bf5
Sunho Kim <ksunhokim123@naver.com>
2020-06-12 03:16:56 +0900

    ci: add fomat-check job

---
+++ .github/format-check.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then 
+    for f in $(git diff --name-only origin/$GITHUB_BASE_REF | grep -E "(vita3k|tools/gen-modules|tools/native-tool)/.*(.cpp|.h)" ); do
+        if [ -f "$f" ]; then
+            if [ "$(diff -u <(cat $f) <(clang-format $f))" != "" ]
+            then
+                echo "run format"
+                exit 1
+            fi
+        fi
+    done
+fi
