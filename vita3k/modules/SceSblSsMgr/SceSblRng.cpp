1a291cc0316f730956d9a763d12162a4e693557a
Francisco José García García <frangarcj@gmail.com>
2020-06-19 18:13:49 +0200

    SceSblRng: Implement _sceKernelGetRandomNumber

--- vita3k/modules/SceSblSsMgr/SceSblRng.cpp
+++ vita3k/modules/SceSblSsMgr/SceSblRng.cpp
@@ -17,8 +17,33 @@
 
 #include "SceSblRng.h"
 
-EXPORT(int, _sceKernelGetRandomNumber) {
-    return UNIMPLEMENTED();
+#include <algorithm>
+#include <random>
+
+#define SCE_RNG_ERROR_INVALID_ARGUMENT 0x810C0000
+
+EXPORT(int, _sceKernelGetRandomNumber, uint64_t *output, unsigned int size) {
+    if (size > 64) {
+        return RET_ERROR(SCE_RNG_ERROR_INVALID_ARGUMENT);
+    }
+
+    thread_local std::random_device dev;
+    thread_local std::mt19937_64 mt(dev());
+    thread_local auto rng = [&]() { return mt(); };
+
+    int repeat = size / sizeof(uint64_t);
+    int remain = size % sizeof(uint64_t);
+
+    std::generate_n(output, repeat, rng);
+
+    if (remain == 0) {
+        return SCE_KERNEL_OK;
+    }
+
+    uint64_t value = rng();
+    char *ptr = reinterpret_cast<char *>(&value);
+    std::copy(ptr, ptr + remain, reinterpret_cast<char *>(&output[repeat]));
+    return SCE_KERNEL_OK;
 }
 
 BRIDGE_IMPL(_sceKernelGetRandomNumber)
