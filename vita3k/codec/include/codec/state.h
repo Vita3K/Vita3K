52ddd44f1c7f0d04d151ead582560b21e45058b6
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2020-07-11 02:11:17 -0400

    codec: add aac decoder.

--- vita3k/codec/include/codec/state.h
+++ vita3k/codec/include/codec/state.h
@@ -19,11 +19,13 @@
 
 #include <array>
 #include <cstdint>
+#include <memory>
 #include <queue>
 #include <string>
 
 struct AVFrame;
 struct AVPacket;
+struct AVIOContext;
 struct AVCodecContext;
 struct AVFormatContext;
 struct AVCodecParserContext;
@@ -163,6 +165,29 @@ public:
     explicit PCMDecoderState(const float dest_frequency);
 };
 
+struct AacDecoderState : public DecoderState {
+    std::shared_ptr<uint8_t> io_buffer;
+    std::shared_ptr<AVIOContext> io_context;
+
+    int stream_id = -1;
+    std::shared_ptr<AVFormatContext> format_context;
+
+    // Stores previous packet data that has not been used yet by the format context.
+    // Temporary workaround for the Codec API.
+    uint32_t packet_coarse_index = 0;
+    uint32_t packet_fine_index = 0;
+    std::vector<std::vector<uint8_t>> packet_cache;
+
+    size_t read_packet_cache(uint8_t *data, size_t size);
+
+    uint32_t get(DecoderQuery query) override;
+
+    bool send(const uint8_t *data, uint32_t size) override;
+    bool receive(uint8_t *data, DecoderSize *size) override;
+
+    explicit AacDecoderState(uint32_t sample_rate, uint32_t channels);
+};
+
 struct PlayerState {
     std::string video_playing;
     std::queue<std::string> videos_queue;
@@ -202,6 +227,7 @@ struct PlayerState {
 };
 
 void convert_yuv_to_rgb(const uint8_t *yuv, uint8_t *rgba, uint32_t width, uint32_t height);
+void convert_f32_to_s16(const float *f32, int16_t *s16, uint32_t dest_channels, uint32_t source_channels, uint32_t samples, uint32_t freq);
 void copy_yuv_data_from_frame(AVFrame *frame, uint8_t *dest);
 std::string codec_error_name(int error);
 bool resample_s16_to_f32(const int16_t *source_s16, int32_t source_channels, uint32_t source_samples, uint32_t source_freq,
