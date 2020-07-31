1a94f91ca71856b643cdfb7c48ff30ee3759b0e4
Sunho Kim <ksunhokim123@naver.com>
2020-07-31 20:22:32 +0900

    config: add console mode option

--- vita3k/config/src/config.cpp
+++ vita3k/config/src/config.cpp
@@ -141,6 +141,8 @@ ExitCode init_config(Config &cfg, int argc, char **argv, const Root &root_paths)
 
     // Grouped options
     auto input = app.add_option_group("Input", "Special options for Vita3K");
+    input->add_option("--console,-z", command_line.console, "Starts the emulator in fullscreen mode.")
+       ->default_val(false)->group("Input");
     input->add_option("--installed-id,-r", command_line.run_title_id, "Title ID of installed app to run")
         ->default_str({})->check(CLI::IsMember(get_file_set(fs::path(cfg.pref_path) / "ux0/app")))->group("Input");
     input->add_option("--recompile-shader,-s", command_line.recompile_shader_path, "Recompile the given PS Vita shader (GXP format) to SPIR_V / GLSL and quit")
