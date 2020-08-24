cd62f50c7b7002ce0aaae82e86d6b93f02ca1b54
sunho <ksunhokim123@naver.com>
2020-08-24 14:34:33 +0900

    renderer: stricter last draw program cache validation
    When the app unregisters program, very rarely, the program address can be the same as the previous program.

--- vita3k/gxm/include/gxm/types.h
+++ vita3k/gxm/include/gxm/types.h
@@ -1128,8 +1128,8 @@ struct GxmContextState {
     Ptr<const SceGxmFragmentProgram> fragment_program;
     Ptr<const SceGxmVertexProgram> vertex_program;
 
-    Ptr<const SceGxmFragmentProgram> last_draw_fragment_program;
-    Ptr<const SceGxmVertexProgram> last_draw_vertex_program;
+    std::string last_draw_fragment_program_hash;
+    std::string last_draw_vertex_program_hash;
 
     // Uniforms.
     UniformBuffers fragment_uniform_buffers;
