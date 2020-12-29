27d7068217a75c3c6b88846a04cd284edb55490b
Zangetsu38 <Dante38@free.fr>
2020-12-29 13:52:53 +0100

    renderer: Add SCE_GXM_COLOR_FORMAT_U5U6U5_RGB

--- vita3k/renderer/src/gl/renderer.cpp
+++ vita3k/renderer/src/gl/renderer.cpp
@@ -494,6 +494,9 @@ void get_surface_data(GLContext &context, size_t width, size_t height, size_t st
     case SCE_GXM_COLOR_FORMAT_U8U8U8_BGR:
         glReadPixels(0, 0, static_cast<GLsizei>(width), static_cast<GLsizei>(height), GL_RGB, GL_UNSIGNED_BYTE, pixels);
         break;
+    case SCE_GXM_COLOR_FORMAT_U5U6U5_RGB:
+        glReadPixels(0, 0, static_cast<GLsizei>(width), static_cast<GLsizei>(height), GL_RGB, GL_UNSIGNED_SHORT_5_6_5, pixels);
+        break;
     case SCE_GXM_COLOR_FORMAT_U8_R:
         glReadPixels(0, 0, static_cast<GLsizei>(width), static_cast<GLsizei>(height), GL_RED, GL_UNSIGNED_BYTE, pixels);
         break;
