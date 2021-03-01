00cd9271d507c528d913e59de2c7a605b11cd3ef
Zangetsu38 <Dante38@free.fr>
2021-03-01 14:44:38 +0100

    gl/renderer: implement COLOR_FORMAT_F32F32_GR on get_surface_data.

--- vita3k/renderer/src/gl/renderer.cpp
+++ vita3k/renderer/src/gl/renderer.cpp
@@ -491,6 +491,9 @@ void get_surface_data(GLContext &context, size_t width, size_t height, size_t st
     case SCE_GXM_COLOR_FORMAT_F16F16F16F16_ABGR:
         glReadPixels(0, 0, static_cast<GLsizei>(width), static_cast<GLsizei>(height), GL_RGBA, GL_HALF_FLOAT, pixels);
         break;
+    case SCE_GXM_COLOR_FORMAT_F32F32_GR:
+        glReadPixels(0, 0, static_cast<GLsizei>(width), static_cast<GLsizei>(height), GL_RG, GL_FLOAT, pixels);
+        break;
     default:
         LOG_ERROR("Color format not implemented: {}, report this to developer", format);
         glReadPixels(0, 0, static_cast<GLsizei>(width), static_cast<GLsizei>(height), GL_RGBA, GL_UNSIGNED_BYTE, pixels);
