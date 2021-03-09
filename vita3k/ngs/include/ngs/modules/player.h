cac52fa80f51b7262f5d3d1229787d0eedc61ae5
mangren <l51492003231@163.com>
2021-03-09 20:27:01 +0100

    codec: Add PCM decoding state
    Not perfect

--- vita3k/ngs/include/ngs/modules/player.h
+++ vita3k/ngs/include/ngs/modules/player.h
@@ -3,16 +3,35 @@
 #include <ngs/system.h>
 
 namespace ngs::player {
+enum {
+    SCE_NGS_PLAYER_CALLBACK_REASON_DONE_ALL = 0,
+    SCE_NGS_PLAYER_CALLBACK_REASON_DONE_ONE_BUFFER = 1,
+    SCE_NGS_PLAYER_CALLBACK_REASON_START_LOOP = 2,
+    SCE_NGS_PLAYER_CALLBACK_REASON_DECODE_ERROR = 3
+};
+
 struct BufferParameter {
     Ptr<void> buffer;
     std::int32_t bytes_count;
     std::int16_t loop_count;
     std::int16_t next_buffer_index;
-    std::int16_t samples_discard_start_off;
-    std::int16_t samples_discard_end_off;
 };
 
 static constexpr std::uint32_t MAX_BUFFER_PARAMS = 4;
+
+enum ParameterAudioType : std::uint8_t {
+    ParameterAudioTypePCM = 0,
+    ParameterAudioTypeADPCM = 1
+};
+
+struct State {
+    std::int32_t current_byte_position_in_buffer;
+    std::int32_t current_buffer;
+    std::int32_t samples_generated_since_key_on;
+    std::int32_t bytes_consumed_since_key_on;
+    std::int32_t total_bytes_consumed;
+    std::int8_t current_loop_count;
+};
 
 struct Parameters {
     ngs::ModuleParameterHeader header;
@@ -24,7 +43,7 @@ struct Parameters {
     std::int32_t start_bytes;
     std::int8_t channels;
     std::int8_t channel_map[2];
-    std::int8_t type;
+    ParameterAudioType type;
     std::int8_t unk60;
     std::int8_t start_buffer;
     std::int8_t unk62;
@@ -32,6 +51,12 @@ struct Parameters {
 };
 
 struct Module : public ngs::Module {
+private:
+    PCMChannelBuf decoded_pending;
+    std::uint32_t decoded_samples_pending;
+    std::uint32_t decoded_passed;
+
+public:
     explicit Module();
     void process(KernelState &kern, const MemState &mem, const SceUID thread_id, ModuleData &data) override;
     void get_expectation(AudioDataType *expect_audio_type, std::int16_t *expect_channel_count) override {}
--- vita3k/ngs/src/modules/player.cpp
+++ vita3k/ngs/src/modules/player.cpp
@@ -1,10 +1,13 @@
 #include <ngs/modules/player.h>
+#include <util/log.h>
 
 #include <cassert>
 
 namespace ngs::player {
 Module::Module()
-    : ngs::Module(ngs::BussType::BUSS_NORMAL_PLAYER) {}
+    : ngs::Module(ngs::BussType::BUSS_NORMAL_PLAYER)
+    , decoded_samples_pending(0)
+    , decoded_passed(0) {}
 
 std::size_t Module::get_buffer_parameter_size() const {
     return sizeof(Parameters);
@@ -12,14 +15,105 @@ std::size_t Module::get_buffer_parameter_size() const {
 
 void Module::process(KernelState &kern, const MemState &mem, const SceUID thread_id, ModuleData &data) {
     Parameters *params = data.get_parameters<Parameters>(mem);
-
-    // TODO: unimplemented, should play audio through sdl I think
+    State *state = data.get_state<State>();
 
     if (data.parent->inputs.inputs.empty()) {
         return;
     }
 
-    assert(data.parent->inputs.inputs.size() == 1);
-    data.parent->products[0] = data.parent->inputs.inputs[0].data();
+    if (state->current_buffer == -1) {
+        return;
+    }
+
+    std::uint8_t *data_ptr = nullptr;
+
+    if (static_cast<std::int32_t>(decoded_samples_pending) < data.parent->rack->system->granularity) {
+        if (!decoded_pending.empty())
+            decoded_pending.erase(decoded_pending.begin(), decoded_pending.begin() + decoded_passed * 4);
+
+        decoded_passed = 0;
+        while (static_cast<std::int32_t>(decoded_samples_pending) < data.parent->rack->system->granularity) {
+            // Ran out of data, supply new
+            // Decode new data and deliver them
+            // Let's open our context
+            auto *input = params->buffer_params[state->current_buffer].buffer.cast<uint8_t>().get(mem)
+                + state->current_byte_position_in_buffer;
+
+            if (params->type == ParameterAudioTypeADPCM) {
+                LOG_ERROR("ADPCM audio, not supported yet!!");
+                return;
+            } else {
+                // TODO: Accelerate
+                std::size_t current_pos = decoded_pending.size();
+
+                std::uint32_t bytes_left_in_buffer = params->buffer_params[state->current_buffer].bytes_count - state->current_byte_position_in_buffer;
+                std::uint32_t samples_to_take = bytes_left_in_buffer / sizeof(std::uint16_t) / params->channels;
+                samples_to_take = std::min<std::uint32_t>(samples_to_take, data.parent->rack->system->granularity);
+
+                bytes_left_in_buffer = samples_to_take * params->channels * sizeof(std::uint16_t);
+                decoded_pending.resize(current_pos + bytes_left_in_buffer);
+
+                if (params->channels == 2) {
+                    std::memcpy(&decoded_pending[current_pos], input, bytes_left_in_buffer);
+                } else {
+                    for (std::size_t i = 0; i < bytes_left_in_buffer; i++) {
+                        decoded_pending[current_pos + 2 * i] = input[i];
+                        decoded_pending[current_pos + 2 * i + 1] = input[i + 1];
+                    }
+                }
+
+                data_ptr = decoded_pending.data();
+
+                state->samples_generated_since_key_on += samples_to_take;
+                state->bytes_consumed_since_key_on += params->channels * samples_to_take * sizeof(std::uint16_t);
+                state->current_byte_position_in_buffer += params->channels * samples_to_take * sizeof(std::uint16_t);
+                state->total_bytes_consumed += params->channels * samples_to_take * sizeof(std::uint16_t);
+
+                decoded_samples_pending += samples_to_take;
+            }
+
+            if (state->current_byte_position_in_buffer >= params->buffer_params[state->current_buffer].bytes_count) {
+                const std::int32_t prev_index = state->current_buffer;
+
+                if (params->buffer_params[state->current_buffer].loop_count != -1) {
+                    state->current_loop_count++;
+
+                    if (state->current_loop_count > params->buffer_params[state->current_buffer].loop_count) {
+                        state->current_buffer = params->buffer_params[state->current_buffer].next_buffer_index;
+                        state->current_loop_count = 0;
+                        state->current_byte_position_in_buffer = 0;
+
+                        data.parent->voice_lock->unlock();
+
+                        if (state->current_buffer == -1) {
+                            data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_DONE_ALL, 0, 0);
+                            // TODO: Free all occupied input routes
+                            //unroute_occupied(mem, voice);
+                        } else {
+                            data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_DONE_ONE_BUFFER, prev_index,
+                                params->buffer_params[state->current_buffer].buffer.address());
+                        }
+
+                        data.parent->voice_lock->lock();
+                    }
+                } else {
+                    data.parent->voice_lock->unlock();
+                    data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_START_LOOP, state->current_loop_count,
+                        params->buffer_params[state->current_buffer].buffer.address());
+                    data.parent->voice_lock->lock();
+                }
+
+                state->current_byte_position_in_buffer = 0;
+            }
+        }
+    }
+
+    std::uint32_t samples_to_be_passed = std::min<std::uint32_t>(decoded_samples_pending, data.parent->rack->system->granularity);
+    data_ptr += 2 * sizeof(std::int16_t) * decoded_passed;
+
+    data.parent->products[0] = data_ptr;
+
+    decoded_samples_pending = (decoded_samples_pending < samples_to_be_passed) ? 0 : (decoded_samples_pending - samples_to_be_passed);
+    decoded_passed += samples_to_be_passed;
 }
 } // namespace ngs::player
