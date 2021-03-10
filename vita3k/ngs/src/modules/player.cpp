dffe3b3c45a1b799a00ab5fbc34b8cab0b8b9815
mangren <l51492003231@163.com>
2021-03-10 19:33:43 +0700

    ngs/scheduler: Fix ear rape continue at end of audio data

--- vita3k/ngs/src/modules/player.cpp
+++ vita3k/ngs/src/modules/player.cpp
@@ -42,8 +42,7 @@ void Module::process(KernelState &kern, const MemState &mem, const SceUID thread
 
         state->decoded_gran_passed = 0;
 
-        while ((static_cast<std::int32_t>(state->decoded_gran_pending) < data.parent->rack->system->granularity)
-            && (state->current_buffer != -1)) {
+        while (static_cast<std::int32_t>(state->decoded_gran_pending) < data.parent->rack->system->granularity) {
             // Ran out of data, supply new
             // Decode new data and deliver them
             // Let's open our context
@@ -75,38 +74,42 @@ void Module::process(KernelState &kern, const MemState &mem, const SceUID thread
 
             state->decoded_gran_pending = samples_to_take_per_channel;
 
-            if (state->current_byte_position_in_buffer >= params->buffer_params[state->current_buffer].bytes_count) {
-                const std::int32_t prev_index = state->current_buffer;
+            if (state->current_buffer == -1) {
+                break;
+            } else {
+                if (state->current_byte_position_in_buffer >= params->buffer_params[state->current_buffer].bytes_count) {
+                    const std::int32_t prev_index = state->current_buffer;
 
-                if (params->buffer_params[state->current_buffer].loop_count != -1) {
-                    state->current_loop_count++;
+                    if (params->buffer_params[state->current_buffer].loop_count != -1) {
+                        state->current_loop_count++;
 
-                    if (state->current_loop_count > params->buffer_params[state->current_buffer].loop_count) {
-                        state->current_buffer = params->buffer_params[state->current_buffer].next_buffer_index;
-                        state->current_loop_count = 0;
-                        state->current_byte_position_in_buffer = 0;
+                        if (state->current_loop_count > params->buffer_params[state->current_buffer].loop_count) {
+                            state->current_buffer = params->buffer_params[state->current_buffer].next_buffer_index;
+                            state->current_loop_count = 0;
+                            state->current_byte_position_in_buffer = 0;
 
-                        data.parent->voice_lock->unlock();
+                            data.parent->voice_lock->unlock();
 
-                        if (state->current_buffer == -1) {
-                            data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_DONE_ALL, 0, 0);
-                            // TODO: Free all occupied input routes
-                            //unroute_occupied(mem, voice);
-                        } else {
-                            data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_DONE_ONE_BUFFER, prev_index,
-                                params->buffer_params[state->current_buffer].buffer.address());
+                            if (state->current_buffer == -1) {
+                                data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_DONE_ALL, 0, 0);
+                                // TODO: Free all occupied input routes
+                                //unroute_occupied(mem, voice);
+                            } else {
+                                data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_DONE_ONE_BUFFER, prev_index,
+                                    params->buffer_params[state->current_buffer].buffer.address());
+                            }
+
+                            data.parent->voice_lock->lock();
                         }
-
+                    } else {
+                        data.parent->voice_lock->unlock();
+                        data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_START_LOOP, state->current_loop_count,
+                            params->buffer_params[state->current_buffer].buffer.address());
                         data.parent->voice_lock->lock();
                     }
-                } else {
-                    data.parent->voice_lock->unlock();
-                    data.invoke_callback(kern, mem, thread_id, SCE_NGS_PLAYER_CALLBACK_REASON_START_LOOP, state->current_loop_count,
-                        params->buffer_params[state->current_buffer].buffer.address());
-                    data.parent->voice_lock->lock();
-                }
 
-                state->current_byte_position_in_buffer = 0;
+                    state->current_byte_position_in_buffer = 0;
+                }
             }
         }
     }
