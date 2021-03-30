fc5b45e11c8a10c711c85874f143937c4f55656f
Zangetsu38 <Dante38@free.fr>
2021-03-30 18:38:36 +0200

    buid: remove select choice visual studio. - vs 2017 is no longer supported.

--- gen-windows.bat
+++ gen-windows.bat
@@ -1,10 +1,5 @@
 @echo off
 
-echo Please choose your preferred Visual Studio version.
-choice /c 97 /n /t 3 /d 7 /m "Press '9' for VS 2019 or '7' for VS 2017: "
-if errorlevel 1 set vs_version=Visual Studio 16 2019
-if errorlevel 2 set vs_version=Visual Studio 15 2017 Win64
-
 REM CI uses pre-built Boost
 IF "%CI%"=="" IF NOT EXIST external\boost-build (
 	REM Create build dir
@@ -24,8 +19,8 @@ pushd build-windows
 
 REM Generate project files
 IF "%CI%"=="" (
-	call cmake -G "%vs_version%" ..
+	call cmake -G "Visual Studio 16 2019" ..
 ) ELSE (
-	call cmake -G "%vs_version%" -DCI:BOOL=ON -DCMAKE_CONFIGURATION_TYPES=%CONFIGURATION% ..
+	call cmake -G "Visual Studio 16 2019" -DCI:BOOL=ON -DCMAKE_CONFIGURATION_TYPES=%CONFIGURATION% ..
 )
 popd
