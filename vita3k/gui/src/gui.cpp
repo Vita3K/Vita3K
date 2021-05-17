bfde83737799a2e4cf91cbccbaae43669a41a912
Zangetsu38 <Dante38@free.fr>
2021-05-17 04:57:52 +0200

    gui: fix trophy dialog in notif and unlock with multiple unlock. - if app unlock few trophy in same time, fix order and double trophy print.

--- vita3k/gui/src/gui.cpp
+++ vita3k/gui/src/gui.cpp
@@ -490,7 +490,7 @@ void init(GuiState &gui, HostState &host) {
     // Initialize trophy callback
     host.np.trophy_state.trophy_unlock_callback = [&gui](NpTrophyUnlockCallbackData &callback_data) {
         const std::lock_guard<std::mutex> guard(gui.trophy_unlock_display_requests_access_mutex);
-        gui.trophy_unlock_display_requests.push(std::move(callback_data));
+        gui.trophy_unlock_display_requests.insert(gui.trophy_unlock_display_requests.begin(), callback_data);
     };
 }
 
