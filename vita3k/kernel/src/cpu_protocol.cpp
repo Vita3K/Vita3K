39306cdcd2a2ad6b3e2033797324170944e7303f
sunho <ksunhokim123@naver.com>
2021-05-24 08:16:28 +0900

    kernel: Space efficient trampoline that modifies only one original instruction

--- vita3k/kernel/src/cpu_protocol.cpp
+++ vita3k/kernel/src/cpu_protocol.cpp
@@ -29,18 +29,41 @@ CPUProtocol::~CPUProtocol() {
 }
 
 void CPUProtocol::call_svc(CPUState &cpu, uint32_t svc, Address pc, SceUID thread_id) {
-    if (svc == TRAMPOLINE_SVC) {
-        Trampoline *tr = *Ptr<Trampoline *>(pc).get(*mem);
-        tr->callback(*kernel, cpu, *mem, tr->lr);
+    // Handle trampoline
+    // 1. Handle trampoline jumper
+    // to save the space we use interrupt to implement jumper
+    // as thumb instructions require total three instructions to jump to any pc without limitations
+    if (svc == TRAMPOLINE_JUMPER_SVC) {
+        // find thumb16 trampoline
+        Trampoline *tr = kernel->debugger.get_trampoline(pc - 2);
+        // find thumb32 or arm trampoline
+        if (!tr)
+            tr = kernel->debugger.get_trampoline(pc - 4);
+        write_pc(cpu, tr->trampoline_addr);
         return;
     }
+
+    // 2. Call trampoline callback
+    // this interrupt is made inside trampoline body
+    if (svc == TRAMPOLINE_HANDLER_SVC) {
+        Trampoline *tr = *Ptr<Trampoline *>(pc).get(*mem);
+        tr->callback(cpu, *mem, tr->lr);
+        return;
+    }
+
+    // This is usual service call
+    uint32_t nid = *Ptr<uint32_t>(pc + 4).get(*mem);
+    call_import(cpu, nid, thread_id);
 
     // TODO: just supply ThreadStatePtr to call_import
     // the only benefit of using thread_id instead--namely less locking--is now gone.
     ThreadStatePtr thread = lock_and_find(thread_id, kernel->threads, kernel->mutex);
-    uint32_t nid = *Ptr<uint32_t>(pc + 4).get(*mem);
-    call_import(cpu, nid, thread_id);
+
+    // Add callback jobs requested inside hle implementation
     thread->flush_callback_requests();
+
+    // ARM recommends claering exclusive state inside interrupt handler
+    clear_exclusive(kernel->exclusive_monitor, get_processor_id(cpu));
 }
 
 Address CPUProtocol::get_watch_memory_addr(Address addr) {
