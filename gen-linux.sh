27e0c030662c04a00addd880bf7375a84f32e121
hobyst <48522412+hobyst@users.noreply.github.com>
2022-05-13 22:15:51 +0200

    Build: Add CMake presets and export compile commands
    - Delete CMake toolchain files in favor of CMake presets
    - Delete gen-macos.sh due to deprecation of macOS support
    - Enable export compile comands for vita3k/ targets
    which makes CMake export the compilation commands for every single
    CMake target under the scope of the vita3k/ directory so that the build
    process can be exposed to other applications such as Cppcheck through
    the compile_commands.json file

--- gen-linux.sh
+++ gen-linux.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # Generate project files for Ninja
-cmake -S . -B build-linux -G Ninja -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain/linux-x64.cmake
+cmake --preset linux-ninja-clang
--- gen-windows.bat
+++ gen-windows.bat
@@ -1,5 +1,5 @@
 @echo off
 
 REM Generate project files for your last Visual Studio version you have
-call cmake -S . -B build-windows -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain/windows-x64.cmake
+call cmake -S . -B build-windows
 pause
