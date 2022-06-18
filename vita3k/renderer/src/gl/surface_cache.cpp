55a6d61f91d481693cc675013c26c664f2743de4
Macdu <Macdu@users.noreply.github.com>
2022-06-18 11:01:16 +0200

    renderer: Fix wrong parameter type for texture filter

--- vita3k/renderer/src/gl/surface_cache.cpp
+++ vita3k/renderer/src/gl/surface_cache.cpp
@@ -150,7 +150,7 @@ std::uint64_t GLSurfaceCache::retrieve_color_surface_texture_handle(const State 
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
                 } else {
-                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                 }
             };
