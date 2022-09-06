77e5c980fea01a5280d1cc514eeccb84f0a15a72
Macdu <Macdu@users.noreply.github.com>
2022-09-07 00:14:01 +0200

    util/fs_utils: unify read_data using SDL_IOFromFile. - Switch all assets (avatars, lang) to use read_data   for universal multi-OS handling. - Fix default icon path check to properly handle assets on all platforms.
    Co-authored-by: Zangetsu38 <dante38@free.fr>

--- vita3k/gui/src/gui.cpp
+++ vita3k/gui/src/gui.cpp
@@ -272,21 +272,36 @@ static void init_font(GuiState &gui, EmuEnvState &emuenv) {
                 fs::path default_font_path = emuenv.static_assets_path / "data/fonts";
 
                 // Check existence of default font file
-                if (fs::exists(default_font_path)) {
-                    gui.vita_font[i] = io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(default_font_path / "mplus-1mn-bold.ttf").c_str(), font_config.SizePixels, &font_config, latin_range);
+                std::vector<uint8_t> font_mplus{};
+                if (fs_utils::read_data(default_font_path / "mplus-1mn-bold.ttf", font_mplus)) {
+                    // when calling AddFontFromMemoryTTF, we tranfer ownership to imgui and it is up to it to free the data
+                    void *font_data = IM_ALLOC(font_mplus.size());
+                    memcpy(font_data, font_mplus.data(), font_mplus.size());
+                    gui.vita_font[i] = io.Fonts->AddFontFromMemoryTTF(font_data, font_mplus.size(), font_config.SizePixels, &font_config, latin_range);
                     font_config.MergeMode = true;
-                    io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(default_font_path / "mplus-1mn-bold.ttf").c_str(), font_config.SizePixels, &font_config, japanese_and_extra_ranges.Data);
+
+                    font_data = IM_ALLOC(font_mplus.size());
+                    memcpy(font_data, font_mplus.data(), font_mplus.size());
+                    io.Fonts->AddFontFromMemoryTTF(font_data, font_mplus.size(), font_config.SizePixels, &font_config, japanese_and_extra_ranges.Data);
 
                     const auto sys_lang = static_cast<SceSystemParamLang>(emuenv.cfg.sys_lang);
-                    if (!emuenv.cfg.initial_setup || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_S) || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_T))
-                        io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(default_font_path / "SourceHanSansSC-Bold-Min.ttf").c_str(), font_config.SizePixels, &font_config, japanese_and_extra_ranges.Data);
+                    if (!emuenv.cfg.initial_setup || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_S) || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_T)) {
+                        std::vector<uint8_t> font_source{};
+                        if (fs_utils::read_data(default_font_path / "SourceHanSansSC-Bold-Min.ttf", font_source)) {
+                            font_data = IM_ALLOC(font_source.size());
+                            memcpy(font_data, font_source.data(), font_source.size());
+                            io.Fonts->AddFontFromMemoryTTF(font_data, font_source.size(), font_config.SizePixels, &font_config, japanese_and_extra_ranges.Data);
+                        }
+                    }
                     font_config.MergeMode = false;
 
                     large_font_config.SizePixels = 134.f;
                     large_font_config.OversampleH = 2;
                     large_font_config.OversampleV = 2;
                     large_font_config.RasterizerDensity = scale;
-                    gui.large_font[i] = io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(default_font_path / "mplus-1mn-bold.ttf").c_str(), large_font_config.SizePixels, &large_font_config, large_font_chars);
+                    font_data = IM_ALLOC(font_mplus.size());
+                    memcpy(font_data, font_mplus.data(), font_mplus.size());
+                    gui.large_font[i] = io.Fonts->AddFontFromMemoryTTF(font_data, font_mplus.size(), large_font_config.SizePixels, &large_font_config, large_font_chars);
 
                     LOG_INFO("Using default Vita3K font.");
                 } else
@@ -312,12 +327,10 @@ vfs::FileBuffer init_default_icon(GuiState &gui, EmuEnvState &emuenv) {
 
     const auto default_fw_icon{ emuenv.pref_path / "vs0/data/internal/livearea/default/sce_sys/icon0.png" };
 
-    fs::path default_icon = emuenv.static_assets_path / "data/image/icon.png";
+    const fs::path default_icon{ emuenv.static_assets_path / "data/image/icon.png" };
 
-    if (fs::exists(default_fw_icon) || fs::exists(default_icon)) {
-        fs::path icon_path = fs::exists(default_fw_icon) ? default_fw_icon : default_icon;
-        fs_utils::read_data(icon_path, buffer);
-    }
+    const fs::path icon_path = fs::exists(default_fw_icon) ? default_fw_icon : default_icon;
+    fs_utils::read_data(icon_path, buffer);
 
     return buffer;
 }
