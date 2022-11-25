fa90e66b9852294b2ff410e396203f6dc7bcad8d
Seungyun Lee <khora.lee.0@gmail.com>
2022-11-26 01:48:31 +0900

    vita3k/updater: Properly implement updater for macOS

---
+++ vita3k/script/update-macos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ ! -e ~/vita3k-latest.dmg ]; then
+    curl -L https://github.com/Vita3K/Vita3K/releases/download/continuous/macos-latest.dmg -o ~/vita3k-latest.dmg
+fi
+
+hdiutil attach ~/vita3k-latest.dmg
+cp -Rf -p /Volumes/Vita3K\ Installer/Vita3K.app $1
+diskutil eject Vita3K\ Installer
+rm -f ~/vita3k-latest.dmg
+xattr -d com.apple.quarantine $1/Vita3K.app
+open $1/Vita3K.app
