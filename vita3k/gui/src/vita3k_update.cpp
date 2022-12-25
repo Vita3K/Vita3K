e1e289187c565729e3e5d31574ff1537bd980b29
Macdu <Macdu@users.noreply.github.com>
2022-12-26 00:07:54 +0100

    updater: Revert commits related to updater (#2246)
    * Revert "vita3k/updater: launch win32 as it's own process."
    
    This reverts commit c6c3dc1c5c3b660147fa25716eae880747df6a07.
    
    * Revert "vita3k/updater: kill Vita3K.exe if boot equals 1"
    
    This reverts commit 21e88d9478d9eee3a386dd6ca6206e76126e02ee.

--- vita3k/gui/src/vita3k_update.cpp
+++ vita3k/gui/src/vita3k_update.cpp
@@ -204,8 +204,7 @@ static void download_update() {
                 SDL_PushEvent(&event);
 
 #ifdef WIN32
-                std::string batch = fmt::format("start \" \" \"{}\\update-vita3k.bat\"", fs::current_path().string());
-                const auto vita3K_batch = batch.c_str();
+                const auto vita3K_batch = "update-vita3k.bat";
 #elif defined(__APPLE__)
                 char *base_path = SDL_GetBasePath();
                 std::string batch = fmt::format("sh {}/update-vita3k.sh {}/../../..", base_path, base_path);
