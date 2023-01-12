0071c734c9250c78b9376061ee9d845eef607039
CoffeeBrewer64 <69767396+CoffeeBrewer64@users.noreply.github.com>
2023-01-12 07:02:24 +0000

    Update update-windows.bat

--- vita3k/script/update-windows.bat
+++ vita3k/script/update-windows.bat
@@ -59,6 +59,6 @@ if exist vita3k-latest.zip (
         echo You can start Vita3K by running Vita3K.exe
     )
 ) else if %boot% EQU 0 (
-    echo Download failed, please run this script wit admin privileges.
+    echo Download failed, please run this script with admin privileges next time.
 )
 pause
