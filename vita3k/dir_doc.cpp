c2c3092d3b59b82937ca41b4e713b29b97153a4e
CoffeeBrewer64 <69767396+CoffeeBrewer64@users.noreply.github.com>
2023-01-12 06:53:42 +0000

    Update dir_doc.cpp

--- vita3k/dir_doc.cpp
+++ vita3k/dir_doc.cpp
@@ -45,7 +45,8 @@
  *
  * The PlayStation Vita has an ARMv7 CPU, making game binaries impossible to run on a x86 system unless recompiled.
  * Vita3K uses the Unicorn JIT recompiler to turn the Vita's ARM code into x86 code in order to enable execution of games in PCs.
- */
+ * ! Unicorn is depracted, using Dynarmic instead.
+*/
 
 /**
  * @dir vita3k/crypto
@@ -89,12 +90,6 @@
  * @dir vita3k/gdbstub
  *
  * @brief GDB integration
- */
-
-/**
- * @dir vita3k/glutil
- *
- * @brief OpenGL shading language implementation
  */
 
 /**
@@ -190,12 +185,6 @@
  * @dir vita3k/renderer
  *
  * @brief OpenGL and Vulkan implementations
- */
-
-/**
- * @dir vita3k/rtc
- *
- * @brief Time and clock implementation
  */
 
 /**
