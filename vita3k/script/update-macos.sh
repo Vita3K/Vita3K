da40e9cca4a4b433395a8c42b09ec294119d88ea
Seungyun Lee <khora.lee.0@gmail.com>
2023-03-30 02:28:14 +0900

    vita3k/updater: Fix broken updater script for macOS
    fix: #2436

--- vita3k/script/update-macos.sh
+++ vita3k/script/update-macos.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-if [ ! -e ~/vita3k-latest.dmg ]; then
-    curl -L https://github.com/Vita3K/Vita3K/releases/download/continuous/macos-latest.dmg -o ~/vita3k-latest.dmg
+if [ ! -e vita3k-latest.dmg ]; then
+    curl -L https://github.com/Vita3K/Vita3K/releases/download/continuous/macos-latest.dmg -o vita3k-latest.dmg
 fi
 
-hdiutil attach ~/vita3k-latest.dmg
-cp -Rf -p /Volumes/Vita3K\ Installer/Vita3K.app $1
+APP_PATH='./../../..'
+
+hdiutil attach vita3k-latest.dmg
+cp -Rf -p /Volumes/Vita3K\ Installer/Vita3K.app $APP_PATH
 diskutil eject Vita3K\ Installer
-rm -f ~/vita3k-latest.dmg
-xattr -d com.apple.quarantine $1/Vita3K.app
-open $1/Vita3K.app
+rm -f vita3k-latest.dmg
+xattr -d com.apple.quarantine $APP_PATH/Vita3K.app
+open $APP_PATH/Vita3K.app
