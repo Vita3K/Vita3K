e457c25ec40c4aa3760cdf8f735ac09fc421702e
Zangetsu38 <dante38@free.fr>
2023-09-13 19:59:10 +0200

    ci: fix format check.

--- .github/format-check.sh
+++ .github/format-check.sh
@@ -3,6 +3,7 @@
 set -ex
 
 if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then 
+    git fetch origin $GITHUB_BASE_REF:refs/remotes/origin/$GITHUB_BASE_REF
     for f in $(git diff --diff-filter=AM --name-only origin/$GITHUB_BASE_REF 'vita3k/**.cpp' 'vita3k/**.h' 'tools/**.cpp' 'tools/**.h'); do
         if [ "$(diff -u <(cat $f) <(clang-format $f))" != "" ]
         then
