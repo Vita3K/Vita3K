6f110b4a953d8fd5b3ee51a7c278aa19ad88759d
Macdu <Macdu@users.noreply.github.com>
2024-05-30 20:21:34 +0200

    modules/SceDisplayUser: stub sceDisplaySetFrameBufInternal.

--- vita3k/modules/SceDriverUser/SceDisplayUser.cpp
+++ vita3k/modules/SceDriverUser/SceDisplayUser.cpp
@@ -61,7 +61,11 @@ EXPORT(int, sceDisplaySetFrameBufForCompat) {
     return UNIMPLEMENTED();
 }
 
-EXPORT(int, sceDisplaySetFrameBufInternal) {
-    TRACY_FUNC(sceDisplaySetFrameBufInternal);
-    return UNIMPLEMENTED();
+EXPORT(int, sceDisplaySetFrameBufInternal, uint32_t maybe_buffer_idx, uint32_t unkn, SceDisplayFrameBuf *pFrameBuf, SceDisplaySetBufSync sync) {
+    TRACY_FUNC(sceDisplaySetFrameBufInternal, maybe_buffer_idx, unkn, pFrameBuf, sync);
+    // only render for frame buffer 0 or we'll get double fps
+    if (maybe_buffer_idx != 0)
+        return 0;
+
+    return CALL_EXPORT(_sceDisplaySetFrameBuf, pFrameBuf, sync, nullptr);
 }
