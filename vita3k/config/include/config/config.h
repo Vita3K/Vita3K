586bbf573800e53ec54254163724191872e8d721
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-08-08 03:44:29 +0800

    Update config.h

--- vita3k/config/include/config/config.h
+++ vita3k/config/include/config/config.h
@@ -83,6 +83,7 @@ enum PerfomanceOverleyPosition {
     code(std::string, "audio-backend", "SDL", audio_backend)                                            \
     code(int, "audio-volume", 100, audio_volume)                                                        \
     code(bool, "ngs-enable", true, ngs_enable)                                                          \
+    code(std::string, "audio-drv", "auto", audio_drv)                                               \
     code(int, "sys-button", static_cast<int>(SCE_SYSTEM_PARAM_ENTER_BUTTON_CROSS), sys_button)          \
     code(int, "sys-lang", static_cast<int>(SCE_SYSTEM_PARAM_LANG_ENGLISH_US), sys_lang)                 \
     code(int, "sys-date-format", (int)SCE_SYSTEM_PARAM_DATE_FORMAT_MMDDYYYY, sys_date_format)           \
