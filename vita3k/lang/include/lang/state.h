de9b4973be8404c8103f1662f81fbca340ce82de
Croden1999 <851577088@qq.com>
2024-08-27 16:28:46 +0800

    lang: Improved error message description.
    Co-authored-by: Macdu <Macdu@users.noreply.github.com>

--- vita3k/lang/include/lang/state.h
+++ vita3k/lang/include/lang/state.h
@@ -46,7 +46,7 @@ struct DialogLangState {
         { "submit", "Submit" },
         { "yes", "Yes" }
     };
-    std::map<std::string, std::string> message = { { "load_app_failed", "Failed to load \"{}\".\nCheck vita3k.log to see console output for details.\n1. Do you have installed firmware?\n2. Dump your own PS Vita app(s)/game(s) and install on Vita3K.\n3. If you want to install or boot Vitamin, it is not supported." } };
+    std::map<std::string, std::string> message = { { "load_app_failed", "Failed to load \"{}\".\nCheck vita3k.log to see console output for details.\n1. Have you installed the firmware?\n2. Re-dump your own PS Vita app/game and install it on Vita3K.\n3. If you want to install or boot Vitamin, it is not supported." } };
     std::map<std::string, std::string> trophy = { { "preparing_start_app", "Preparing to start the application..." } };
     struct SaveData {
         std::map<std::string, std::string> deleting = {
