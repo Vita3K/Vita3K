d09223ef9977f20b0514ea25b94cee889c580380
scribam <scribam@users.noreply.github.com>
2024-09-15 13:16:20 +0200

    util: Add WIN32_LEAN_AND_MEAN before <Windows.h>

--- vita3k/util/src/logging.cpp
+++ vita3k/util/src/logging.cpp
@@ -18,6 +18,7 @@
 #include <util/log.h>
 
 #ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #endif
 
--- vita3k/util/src/vc_runtime_checker.cpp
+++ vita3k/util/src/vc_runtime_checker.cpp
@@ -5,6 +5,7 @@
 
 #include <cstdint>
 
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <shellapi.h>
 #include <winver.h>
