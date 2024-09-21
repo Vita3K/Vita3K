f52b3af7b44d4597219bfc236721c61b3262a1b3
Macdu <Macdu@users.noreply.github.com>
2024-09-21 13:59:52 +0200

    renderer: Improve depth buffer accuracy
    Co-authored-by: SaturnSky <ikadro@gmail.com>

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -616,7 +616,6 @@ bool VKState::create(std::unique_ptr<renderer::State> &state, const Config &conf
 
         // use these features (because they are used by the vita GPU) if they are available
         vk::PhysicalDeviceFeatures enabled_features{
-            .depthClamp = physical_device_features.depthClamp,
             .fillModeNonSolid = physical_device_features.fillModeNonSolid,
             .wideLines = physical_device_features.wideLines,
             .samplerAnisotropy = physical_device_features.samplerAnisotropy,
