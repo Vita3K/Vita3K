0c80d95e55908896afa23e7bc1dcfc687240ba0b
Carlos Estrague  / Mrc_munir <maximu@gmail.com>
2025-03-13 23:51:23 +0100

    Update Appimage arch type build_updater

--- appimage/build_updater.sh
+++ appimage/build_updater.sh
@@ -3,6 +3,17 @@
 # Note: The documentation at https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/blob/master/README.md for embedding update info is wrong.
 # The correct env-variable name to use is UPDATE_INFORMATION, and *not* LDAI_UPDATE_INFORMATION.
 
-echo "Executing linuxdeploy with cmdline: LDAI_VERBOSE=1 UPDATE_INFORMATION=\"gh-releases-zsync|Vita3K|Vita3K|latest|Vita3K-x86_64.AppImage.zsync\" $@"
 
-LDAI_VERBOSE=1 UPDATE_INFORMATION="gh-releases-zsync|Vita3K|Vita3K|latest|Vita3K-x86_64.AppImage.zsync" $@
+ARCH=$(uname -m)
+
+#detec arch
+if [ "$ARCH" = "x86_64" ]; then
+    UPDATE_INFO="gh-releases-zsync|Vita3K|Vita3K|latest|Vita3K-x86_64.AppImage.zsync"
+elif [ "$ARCH" = "aarch64" ]; then
+    UPDATE_INFO="gh-releases-zsync|Vita3K|Vita3K|latest|Vita3K-aarch64.AppImage.zsync"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+LDAI_VERBOSE=1 UPDATE_INFORMATION="$UPDATE_INFO" $@
