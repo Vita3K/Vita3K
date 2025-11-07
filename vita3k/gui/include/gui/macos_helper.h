c75a20b839f8221397c5e243934d108aff0a848d
Zangetsu38 <dante38@free.fr>
2025-11-07 06:41:24 +0100

    gui/app_context_menu: Add support for creating app shortcuts. - Windows: Convert icon0.png to .ico, store in cache/icons, and create .lnk shortcut. - macOS: Create a desktop app bundle with launcher script and icon. - Linux: Copy icon0.png to cache/icons and create .desktop shortcut on desktop. - Android: Call Java createShortcut() via JNI. lang: Add 'Create Shortcut' in en/fr.
    Co-authored-by: Seungyun Lee <khora.lee.0@gmail.com>
    Co-authored-by: Macdu <Macdu@users.noreply.github.com>

---
+++ vita3k/gui/include/gui/macos_helper.h
@@ -0,0 +1,25 @@
+// Vita3K emulator project
+// Copyright (C) 2025 Vita3K team
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#pragma once
+
+namespace gui {
+bool png_to_icns(const fs::path &png_path, const fs::path &icns_path);
+fs::path get_desktop_path();
+bool create_launcher(const fs::path &exe_path, const fs::path &macOS_path, const std::string &app_path);
+bool create_info_plist(const fs::path &contents_path, const std::string &app_name, const std::string &title_id);
+} // namespace gui
