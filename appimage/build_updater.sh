cff5091d0e3a0a997dc7c91bc905a1985efb8a98
Carlos Estrague  / Mrc_munir <maximu@gmail.com>
2026-01-03 11:58:09 +0100

    feat: Add ARM64 Linux support for Vita3K
    - Update build_updater.sh for multi-arch compatibility
    - Update vita3k_update.cpp for Linux ARM64 support
    - Add CI workflows for ARM64 zip and AppImage builds

--- appimage/build_updater.sh
+++ appimage/build_updater.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
 # Note: The documentation at https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/blob/master/README.md for embedding update info is wrong.
+# Detect architecture
 # The correct env-variable name to use is UPDATE_INFORMATION, and *not* LDAI_UPDATE_INFORMATION.
 
-echo "Executing linuxdeploy with cmdline: LDAI_VERBOSE=1 UPDATE_INFORMATION=\"gh-releases-zsync|Vita3K|Vita3K|latest|Vita3K-x86_64.AppImage.zsync\" $@"
+# Detect architecture
+ARCH=$(uname -m)
 
-LDAI_VERBOSE=1 UPDATE_INFORMATION="gh-releases-zsync|Vita3K|Vita3K|latest|Vita3K-x86_64.AppImage.zsync" $@
+if [ "$ARCH" = "x86_64" ]; then
+    APPIMAGE_NAME="Vita3K-x86_64.AppImage"
+elif [ "$ARCH" = "aarch64" ]; then
+    APPIMAGE_NAME="Vita3K-aarch64.AppImage"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+echo "Executing linuxdeploy with cmdline: LDAI_VERBOSE=1 UPDATE_INFORMATION=\"gh-releases-zsync|Vita3K|Vita3K|latest|${APPIMAGE_NAME}.zsync\" $@"
+
+LDAI_VERBOSE=1 UPDATE_INFORMATION="gh-releases-zsync|Vita3K|Vita3K|latest|${APPIMAGE_NAME}.zsync" $@
