25f4d42f76716b86da9efbe756706014807c7521
nn9dev <39563889+nn9dev@users.noreply.github.com>
2026-09-15 16:18:07 -0400

    sceTouch: make touch_get not return 0
    This one ACTUALLY fixes Disgaea 3 crashing. Disgaea 3 reads the touch panels every frame and uses the returned buffer count as a loop bound, indexing record[count - 1] without guarding for count == 0 (since the function should never return 0). I think the emuenv.drop_inputs returning 0 due to the save file commondialog was making the bug proliferate and attempt to continue to read touch data but would run past the touch data and make the game hang

--- vita3k/modules/SceAppUtil/SceAppUtil.cpp
+++ vita3k/modules/SceAppUtil/SceAppUtil.cpp
@@ -36,6 +36,7 @@
 #include <unistd.h>
 #endif
 
+#include <algorithm>
 #include <cstring>
 
 TRACY_MODULE_NAME(SceAppUtil);
@@ -540,27 +541,25 @@ EXPORT(SceInt32, sceAppUtilSystemParamGetInt, SceSystemParamId paramId, SceInt32
 
 EXPORT(int, sceAppUtilSystemParamGetString, unsigned int paramId, SceChar8 *buf, SceSize bufSize) {
     TRACY_FUNC(sceAppUtilSystemParamGetString, paramId, buf, bufSize);
-    int copySize = bufSize;
     switch (paramId) {
     case SCE_SYSTEM_PARAM_ID_USER_NAME: {
         if (bufSize == 0) {
             return RET_ERROR(SCE_APPUTIL_ERROR_PARAMETER);
         }
         // copy user name into buf with size SCE_SYSTEM_PARAM_USERNAME_MAXSIZE or bufSize, whichever is smaller
-        if (bufSize > SCE_SYSTEM_PARAM_USERNAME_MAXSIZE)
-            copySize = SCE_SYSTEM_PARAM_USERNAME_MAXSIZE;
+        const auto copySize = std::min(bufSize, static_cast<unsigned int>(SCE_SYSTEM_PARAM_USERNAME_MAXSIZE));
         std::strncpy(reinterpret_cast<char *>(buf), emuenv.io.user_name.c_str(), copySize);
         buf[copySize - 1] = '\0';
         // fallback to hostname
-        if (*buf == 0) {
+        if (buf[0] == '\0') {
             char devname[SCE_SYSTEM_PARAM_USERNAME_MAXSIZE];
-            if(gethostname(devname, SCE_SYSTEM_PARAM_USERNAME_MAXSIZE) == 0) {
+            if (gethostname(devname, SCE_SYSTEM_PARAM_USERNAME_MAXSIZE) == 0) {
                 std::strncpy(reinterpret_cast<char *>(buf), devname, copySize);
                 buf[copySize - 1] = '\0';
             }
         }
         // double fallback to "Vita3K"
-        if (*buf == 0) {
+        if (buf[0] == '\0') {
             std::strncpy(reinterpret_cast<char *>(buf), "Vita3K", copySize);
             buf[copySize - 1] = '\0';
         }
